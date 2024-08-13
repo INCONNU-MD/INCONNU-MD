a4f10a1dd9a01a5cf59912190a0bdee5ca188e80
INCONNU-MD <ha9384761@gmail.com>
2024-08-13 17:33:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
-console.log("Connecting inconnu V2 MD ðŸ§¬...");
+console.log("Connecting inconnu-MD ðŸ§¬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
 
